1374df9725762c5835f7a2ff88795d35092f275e
notarazicom <notarazi.com@gmail.com>
2018-09-18 23:52:54 +0800

    Add files via upload

---
+++ www/js/controllers.js
@@ -0,0 +1,57 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$ionicPlatform', '$location', 	function($scope, $ionicPlatform, $location) {
+
+	// init gps array
+    $scope.whoiswhere = [];
+    $scope.basel = { lat: 47.55633987116614, lon: 7.576619513223015 };
+
+
+    // check login code
+	$ionicPlatform.ready(function() {	navigator.geolocation.getCurrentPosition(function(position) {
+		    $scope.position=position;
+	        var c = position.coords;
+	        $scope.gotoLocation(c.latitude, c.longitude);
+		    $scope.$apply();
+		    },function(e) { console.log("Error retrieving position " + e.code + " " + e.message) });
+	    $scope.gotoLocation = function (lat, lon) {
+	        if ($scope.lat != lat || $scope.lon != lon) {
+	            $scope.basel = { lat: lat, lon: lon };
+	            if (!$scope.$$phase) $scope.$apply("basel");
+				}
+			};
+
+		    // some points of interest to show on the map
+		    // to be user as markers, objects should have "lat", "lon", and "name" properties
+		    $scope.whoiswhere = [
+		        { "name": "My Marker", "lat": $scope.basel.lat, "lon": $scope.basel.lon },
+				];
+
+			});
+
+}])
+   
+.controller('cartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
